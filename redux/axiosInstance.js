f6e6c0fa229fa3d0fc5b5d37cbde945e22b5241b
RisvanBCA2022 <111649025+RisvanBCA2022@users.noreply.github.com>
2023-12-02 05:36:51 +0530

    final push

--- redux/axiosInstance.js
+++ redux/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://codecore.rzspyboy.me/',
+  baseURL: 'https://codecoresite.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
   },
